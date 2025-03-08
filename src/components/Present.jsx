4a07daa7d7abc2543b7eb10878fed0ff393b4d7f
aomaryoussef <abdelaziz.youssef@btech.com>
2025-03-08 02:07:54 +0200

    fix: update raffle prize descriptions in Present component

--- src/components/Present.jsx
+++ src/components/Present.jsx
@@ -16,17 +16,17 @@ function Present() {
           <div className="gift-bottom"></div>
           <div id="raffle-red" className="entry raffle raffle-1">
             <div className="no-scale">
-              <span className="ticket-text">Free<br />Dinner</span>
+              <span className="ticket-text">Perfum</span>
             </div>
           </div>
           <div id="raffle-red" className="entry raffle raffle-2">
             <div className="no-scale">
-              <span className="ticket-text">Free<br />Massage</span>
+              <span className="ticket-text">Silver<br />Nicklace</span>
             </div>
           </div>
           <div id="raffle-red" className="entry raffle raffle-3">
             <div className="no-scale">
-              <span className="ticket-text">Free<br />Dinner</span>
+              <span className="ticket-text">Shoes<br />(Kotchi)</span>
             </div>
           </div>
         </label>
